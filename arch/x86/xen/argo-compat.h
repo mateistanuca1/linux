2abbef209f794939b2d68f4c0c06bae91350874e
Matei Stanuca <stanucamatei@gmail.com>
2026-03-28 22:53:49 +0000

    Added argo ring primitives
    Signed-off-by: Matei Stanuca <stanucamatei@gmail.com>

---
+++ arch/x86/xen/argo-compat.h
@@ -0,0 +1,52 @@
+#include <xen/page.h>
+#include <xen/events.h>
+#include <asm/xen/hypercall.h>
+#include <xen/xen.h>
+
+#ifndef HYPERVISOR_argo_op
+#define __HYPERVISOR_argo_op	39
+
+#ifndef argo_op
+#define argo_op __HYPERVISOR_argo_op
+#endif
+
+#ifndef _hypercall5
+/*
+ * Removed because unused in:
+ * https://lkml.org/lkml/2018/8/20/267
+ */
+#define _hypercall5(type, name, a1, a2, a3, a4, a5)	\
+({							\
+	__HYPERCALL_DECLS;				\
+	__HYPERCALL_5ARG(a1, a2, a3, a4, a5);		\
+	asm volatile (__HYPERCALL			\
+		: __HYPERCALL_5PARAM			\
+		: __HYPERCALL_ENTRY(name)		\
+		: __HYPERCALL_CLOBBER5);		\
+	(type)__res;					\
+})
+#endif	/* _hypercall5*/
+
+#if ( LINUX_VERSION_CODE < KERNEL_VERSION(5,2,0) )
+#define __xen_stac() stac()
+#define __xen_clac() clac()
+#endif
+
+static inline int __must_check
+HYPERVISOR_argo_op(int cmd, void *arg1, void *arg2, uint32_t arg3,
+	uint32_t arg4)
+{
+	int ret;
+
+	__xen_stac();
+	ret = _hypercall5(int, argo_op, cmd, arg1, arg2, arg3, arg4);
+	__xen_clac();
+
+	return ret;
+}
+
+#ifndef VIRQ_ARGO
+#define VIRQ_ARGO   11 /* G. (DOM0) ARGO interdomain communication */
+#endif
+
+#endif /* _ARGO_H_ */
---
+++ include/xen/argo/argo_ring.h
@@ -0,0 +1,78 @@
+#ifndef _ARGO_RING_H_
+# define _ARGO_RING_H_
+
+#include <linux/skbuff.h>
+
+#include <xen/argo/argo.h>
+
+/*
+ * Ring GFN management.
+ */
+struct argo_gfn_array {
+	size_t n;
+	xen_argo_gfn_t gfns[0];
+};
+
+/*
+ * xen_argo_ring_t does not have a length field.
+ * Arbitrary fixed ring size for now.
+ * This size is chosen to match other Argo driver implementations.
+ */
+static const size_t ring_len = 32 * PAGE_SIZE;
+
+/*
+ * Messages on the ring are aligned on XEN_ARGO_MSG_SLOT_SIZE.
+ * XEN_ARGO_MSG_SLOT_SIZE needs to be a power of 2.
+ */
+#define ARGO_RING_ALIGN(a) round_up((a), XEN_ARGO_MSG_SLOT_SIZE)
+
+static inline void argo_ring_check_sizes(void)
+{
+	BUILD_BUG_ON_NOT_POWER_OF_2(XEN_ARGO_MSG_SLOT_SIZE);
+}
+
+/*
+ * Ring management.
+ */
+struct list_head argo_rings;
+rwlock_t argo_rings_lock;
+typedef int (*argo_recv_data_cb)(void *priv, void *data);
+struct argo_ring_hnd {
+	struct list_head l;
+	spinlock_t ring_lock;
+	xen_argo_ring_t *ring;
+	unsigned int ring_len;
+	struct argo_gfn_array *gfns;
+	xen_argo_port_t aport;
+	domid_t partner_id;
+	argo_recv_data_cb recv_cb;
+	void *priv;	/* TODO: Do better. Opaque to get struct
+			   vsock_sock/struct sock to recv_cb */
+};
+
+/*
+ * Ring handle primitives.
+ */
+void argo_ring_handle_free(struct argo_ring_hnd *h);
+struct argo_ring_hnd *argo_ring_handle_alloc(domid_t domain, unsigned int port,
+		argo_recv_data_cb recv_cb, void *priv);
+inline size_t argo_ring_has_data(const struct argo_ring_hnd *h);
+inline size_t argo_ring_has_space(const struct argo_ring_hnd *h);
+/*
+ * Ring primitives, hypercalls to Xen.
+ */
+void argo_ring_unregister(struct argo_ring_hnd *h);
+int argo_ring_register(struct argo_ring_hnd *h);
+bool argo_ring_exists(domid_t domain, unsigned int port);
+domid_t argo_get_local_cid(void);
+/*
+ * Ring "send" primitive. send is synchronous, direct hypercall to Xen.
+ */
+int argo_ring_send(struct argo_ring_hnd *h, xen_argo_iov_t *iov,
+		xen_argo_send_addr_t *send, uint32_t msg_type);
+int argo_ring_recv(struct argo_ring_hnd *h, void *buf, size_t len);
+
+int argo_core_init(irqreturn_t (*argo_vsock_interrupt)(int, void *));
+void argo_core_cleanup(void);
+
+#endif /* !_ARGO_RING_H_ */
